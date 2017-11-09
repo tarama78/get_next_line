5b837968df041552703f1f7ce16ddc49628930f7
Timothee NICOLAS <tnicolas@e2r8p3.42.fr>
2017-11-09 11:28:01 +0100

    create files

---
+++ main.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tnicolas <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/11/09 10:58:58 by tnicolas          #+#    #+#             */
+/*   Updated: 2017/11/09 11:27:53 by tnicolas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <limits.h>
+#include <string.h>
+#include <ctype.h>
+#include <get_next_line.h>
+
+int				main(int ac, char **av)
+{
+	int			i;
+
+	(void)ac;
+	(void)av;
+	(void)i;
+	
+	return (0);
+}
