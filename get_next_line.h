9ed1709de61fd9cb5133691184b0dc6a00636f45
tim <tnicolas@student.42.fr>
2017-11-20 19:50:26 +0100

    ok

--- get_next_line.h
+++ get_next_line.h
@@ -1,30 +1,18 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: tnicolas <marvin@42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/11/09 10:47:08 by tnicolas          #+#    #+#             */
-/*   Updated: 2017/11/10 10:22:50 by tnicolas         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# define BUFF_SIZE 64
+# define BUFF_SIZE 2048
 # define END 0
 # define LINE_READ 1
+# define SUCCESS 1
 # define ERROR -1
 
-typedef struct	s_file
+typedef struct			s_file
 {
-	int			fd;
-	int			line;
-	char		*content;
-}				t_file;
+	int					fd;
+	char				*line_fd;
+}						t_file;
 
-int				get_next_line(const int fd, char **line);
+int						get_next_line(const int fd, char **line);
 
 #endif
